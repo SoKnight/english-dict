c8511897b697246c50adc4863bde7739895d9fff
SoKnight <mcliyanage2018@gmail.com>
2023-10-05 11:19:09 +0300

    Change stroke width of word examples icon

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -73,7 +73,7 @@ function generateWordCard(wordModel) {
     let examples = wordModel[6];
     if (examples && examples.length % 2 === 0) {
         let node = createElement("div", "examples");
-            node.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke-width=\"1\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-book-open\"><path d=\"M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z\"/><path d=\"M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z\"/></svg>";
+            node.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke-width=\"0.0625rem\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"feather feather-book-open\"><path d=\"M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z\"/><path d=\"M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z\"/></svg>";
 
         let listNode = createElement("div", "list");
         for (let i = 0; i < examples.length; i++) {
